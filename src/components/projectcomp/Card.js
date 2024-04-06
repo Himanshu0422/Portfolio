d5f9b2dcf08d3fdec27db394683849a470ab5918
Himanshu0422 <himanshumittal035@gmail.com>
2024-04-06 12:17:52 +0530

    ui changes

--- src/components/projectcomp/Card.js
+++ src/components/projectcomp/Card.js
@@ -5,14 +5,14 @@ const Card = ({data, projectId}) => {
     const project = data[projectId];
 
     return (
-        <div className='card p-5 rounded-lg flex flex-col gap-2'>
+        <div className='card p-6 rounded-lg flex flex-col gap-4'>
             <img src={project?.image} alt='coding' className='h-56 max-sm:w-[15rem] w-[18rem]' />
-            <div className='flex flex-col justify-center items-center'>
+            <div className='flex flex-col justify-center items-center gap-2'>
                 <div className='text-lg'>{project?.name}</div>
                 <div className='text-sm'>{project?.description}</div>
-                <div className='flex gap-5'>
-                    <a href={project?.demo}>Demo</a>
-                    <a href={project?.github}>Source Code</a>
+                <div className='flex gap-5 text-2xl'>
+                    <a href={project?.demo}><i class="fa-solid fa-play"></i></a>
+                    <a href={project?.github}><i class="fa-brands fa-github"></i></a>
                 </div>
             </div>
         </div>
